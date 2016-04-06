a7e96dce8403fdc687dff3512657114b02f195c3
Tristan Rice <rice@fn.lc>
2016-04-06 16:03:31 -0700

    Added wall sides

--- p4/js/objects.js
+++ p4/js/objects.js
@@ -1,15 +1,30 @@
 const WALL_COLOR = 0x3d3d3d;
+const WALL_SIDE_COLOR = 0x999999;
 const TURRET_BASE_COLOR = WALL_COLOR;
-const TURRET_BARREL_COLOR = 0x999999;
+const TURRET_BARREL_COLOR = WALL_SIDE_COLOR;
 
 const TURRET_LASER_COLOR = 0xff0000;
 
 function Wall() {
-  this.geometry = new THREE.BoxGeometry( 1, 1, 1 );
+  this.geometry = new THREE.BoxGeometry( 0.5, 1, 0.5 );
   this.material = new THREE.MeshLambertMaterial( { color: WALL_COLOR } );
   this.object = new THREE.Mesh( this.geometry, this.material );
   this.object.controller = this;
   this.object.position.y = cursorElevation() + 0.5;
+
+  var sideGeometry = new THREE.BoxGeometry( 0.5, 0.8, 0.3 );
+  var sideMaterial = new THREE.MeshLambertMaterial( { color: WALL_SIDE_COLOR } );
+	for (var i=0; i<4; i++) {
+		var angle = Math.PI*i/2;
+		var x = Math.sin(angle)/4;
+		var z = Math.cos(angle)/4;
+		var side = new THREE.Mesh( sideGeometry, sideMaterial );
+		side.position.x = x;
+		side.position.z = z;
+		side.position.y = -0.1;
+		side.rotateY(Math.PI/2 + angle);
+		this.object.add(side);
+	}
 }
 Wall.cost = 100;
 
